c5a735ea61f9aa81af9b3ec50b219b1fbcd1c237
Tim Kingston <timkingston312@gmail.com>
2023-03-23 12:05:10 -0400

    loopingLab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names = ["Guadalupe","Ollie","Aki"];
+const newNames=[];
+
+function writeCards(names){
+    for (let i = 0; i < names.length; i++) {
+newNames.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`) ;
+    }
+return newNames; 
+}
+
+let n=10;
+function countDown(n){
+    while(n>-1){
+        console.log(n);
+        n=n-1;
+    }
+}
+
